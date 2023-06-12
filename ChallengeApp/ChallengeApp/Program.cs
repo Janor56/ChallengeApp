2ac8756b644229a0994d06dfb390fe502c3d5aa3
Janusz Kiszczak <janor56@wp.pl>
2023-06-13 01:35:31 +0200

    Kurs C# od Podstaw, zadanie domowe dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.ComponentModel.Design;
+
+var imię = "Ewa";
+var płeć = "kobieta";
+var wiek = 20;
+
+if (płeć == "kobieta")
+{
+    if (imię == "Ewa" && wiek == 33)
+    {
+        Console.WriteLine("Ewa, lat 33");
+    }
+    else if (wiek < 30)
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+    else
+    {
+        Console.WriteLine("Kobieta 30 lat wzwyż");
+    }
+}
+else
+{
+    if (wiek < 18)
+    {
+        Console.WriteLine("Niepełnoletni mężczyzna");
+    }
+    else
+    {
+        if (imię == "Tomasz")
+            {
+               if (wiek >= 30)
+               {
+                 Console.WriteLine("Tomasz, 30 lat wzwyż");
+               }
+              else
+               {
+                 Console.WriteLine("Tomasz, mniej niż 30 lat");
+               }
+            }
+        else
+        {
+            Console.WriteLine("Pełnoletni mężczyzna");
+        }
+    }
+}
+
+    
+
