b612a39553bd4cb24f6308dba7ef375ea900f23d
Janusz Kiszczak <janor56@wp.pl>
2023-06-16 23:45:30 +0200

    Dzień 5 - zadanie domowe

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,49 +1,72 @@
-﻿using System.ComponentModel.Design;
+﻿int number = 770266;
+string numberInString = number.ToString();
+char[] letters = numberInString.ToArray();
 
-var imię = "Ewa";
-var płeć = "kobieta";
-var wiek = 20;
+Console.WriteLine("Liczba wynosi:" + number);
 
-if (płeć == "kobieta")
+int counter0 = 0;
+int counter1 = 0;
+int counter2 = 0;
+int counter3 = 0;
+int counter4 = 0;
+int counter5 = 0;
+int counter6 = 0;
+int counter7 = 0;
+int counter8 = 0;
+int counter9 = 0;
+
+foreach (char letter in letters)
 {
-    if (imię == "Ewa" && wiek == 33)
+    if (letter == '0')
     {
-        Console.WriteLine("Ewa, lat 33");
+        counter0++;
     }
-    else if (wiek < 30)
+    else if (letter == '1')
     {
-        Console.WriteLine("Kobieta poniżej 30 lat");
+        counter1++;
     }
-    else
+    else if (letter == '2')
     {
-        Console.WriteLine("Kobieta 30 lat wzwyż");
+        counter2++;
+    }
+    else if (letter == '3')
+    {
+        counter3++;
+    }
+    else if (letter == '4')
+    {
+        counter4++;
+    }
+    else if (letter == '5')
+    {
+        counter5++;
+    }
+    else if (letter == '6')
+    {
+        counter6++;
+    }
+    else if (letter == '7')
+    {
+        counter7++;
+    }
+    else if (letter == '8')
+    {
+        counter8++;
+    }
+    else if (letter == '9')
+    {
+        counter9++;
     }
 }
-else
-{
-    if (wiek < 18)
-    {
-        Console.WriteLine("Niepełnoletni mężczyzna");
-    }
-    else
-    {
-        if (imię == "Tomasz")
-            {
-               if (wiek >= 30)
-               {
-                 Console.WriteLine("Tomasz, 30 lat wzwyż");
-               }
-              else
-               {
-                 Console.WriteLine("Tomasz, mniej niż 30 lat");
-               }
-            }
-        else
-        {
-            Console.WriteLine("Pełnoletni mężczyzna");
-        }
-    }
-}
 
-    
+Console.WriteLine("0 => " + counter0);
+Console.WriteLine("1 => " + counter1);
+Console.WriteLine("2 => " + counter2);
+Console.WriteLine("3 => " + counter3);
+Console.WriteLine("4 => " + counter4);
+Console.WriteLine("5 => " + counter5);
+Console.WriteLine("6 => " + counter6);
+Console.WriteLine("7 => " + counter7);
+Console.WriteLine("8 => " + counter8);
+Console.WriteLine("9 => " + counter9);
 
